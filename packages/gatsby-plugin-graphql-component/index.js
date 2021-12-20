ecee49357fbc5a01d74be7e714e0c1efb11f2c5a
ardiewen <ardie.wen@hunterlily.co>
2021-12-20 14:31:56 -0500

    fix: replace runQuery with getNodeById

--- packages/gatsby-plugin-graphql-component/index.js
+++ packages/gatsby-plugin-graphql-component/index.js
@@ -11,11 +11,11 @@ const pascalCase = _.flow(_.camelCase, _.upperFirst)
 let actions
 let store
 
-exports.setActions = options => {
+exports.setActions = (options) => {
   actions = options.actions
 }
 
-exports.setStore = options => {
+exports.setStore = (options) => {
   store = options.store
 }
 
@@ -25,25 +25,19 @@ exports.setStore = options => {
 exports.registerComponent = async ({ component }) => {
   const writeDirectory = await ensureWriteDirectory({
     baseDirectory: store.getState().program.directory,
-    paths: [`components`]
+    paths: [`components`],
   })
 
   const id = createNodeId(`graphql-component-source-${component}`, name)
   const componentPath = path.join(writeDirectory, `${id}.js`)
 
-  // TODO: Remove all "hot" references in this `syncRequires` variable when fast-refresh is the default
-  const hotImport =
-    process.env.GATSBY_HOT_LOADER !== `fast-refresh` ? `const { hot } = require("react-hot-loader/root")` : ``
-  const hotMethod = process.env.GATSBY_HOT_LOADER !== `fast-refresh` ? `hot` : ``
-
   await writeFile({
     filePath: componentPath,
     data: `
-  ${hotImport}
   import Component from "${component}"
 
-  export default ${hotMethod}(Component)
-  `
+  export default Component
+  `,
   })
 
   const node = {
@@ -53,8 +47,8 @@ exports.registerComponent = async ({ component }) => {
     componentName: `Component${pascalCase(component)}`,
     componentChunkName: generateComponentChunkName(component),
     internal: {
-      type: `GraphQLComponentSource`
-    }
+      type: `GraphQLComponentSource`,
+    },
   }
 
   node.internal.contentDigest = createContentDigest(JSON.stringify(node))
@@ -71,16 +65,9 @@ exports.createResolverField = ({ resolve }) => {
   return {
     type: `GraphQLComponent`,
     async resolve(source, args, context, info) {
-      const node = await context.nodeModel.runQuery({
-        query: {
-          filter: {
-            id: {
-              eq: await resolve(source, args, context, info)
-            }
-          }
-        },
+      const node = await context.nodeModel.getNodeById({
+        id: await resolve(source, args, context, info),
         type: `GraphQLComponentSource`,
-        firstOnly: true
       })
 
       if (process.env.NODE_ENV !== `production`) {
@@ -93,10 +80,10 @@ exports.createResolverField = ({ resolve }) => {
             ___graphQLComponent: {
               componentChunkName: node.componentChunkName,
               componentPath: node.componentPath,
-              componentName: node.componentName
-            }
+              componentName: node.componentName,
+            },
           }
         : null
-    }
+    },
   }
 }
